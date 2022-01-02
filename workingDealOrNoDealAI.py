80292507cbf99037f0abcf0e4fff7c44247123bc
Tom Chambers <54739051+ThomasChambers15243@users.noreply.github.com>
2022-01-02 18:27:59 +0000

    Add files via upload

---
+++ workingDealOrNoDealAI.py
@@ -0,0 +1,29 @@
+import random
+import os
+import time
+
+
+while True:#27
+    NeuralNetwork = []
+    for i in range(1,27):
+        NeuralNetwork.append(i)
+
+    print("Press any key to get a good box number.\nMake sure to select the given box")
+
+    while len(NeuralNetwork) >= 25:
+        #print(str(len(NeuralNetwork)))
+        sInput = input("Press any key for number...\n")
+        os.system('cls')
+        print("AI CALCULATES...")
+        time.sleep(0.5)
+        print("...")
+        time.sleep(0.5)
+        print("...")
+        time.sleep(0.5)
+        print("AI DECIDES:")
+        randint = random.randint(0,7)
+        print(NeuralNetwork
+        [randint])
+        print(":)\n")
+        NeuralNetwork.pop(randint)
+		
