81b9be3255de88730786ea8f055ceeb219965697
Tom Chambers <54739051+ThomasChambers15243@users.noreply.github.com>
2022-01-02 18:28:16 +0000

    Update workingDealOrNoDealAI.py

--- workingDealOrNoDealAI.py
+++ workingDealOrNoDealAI.py
@@ -10,7 +10,7 @@ while True:#27
 
     print("Press any key to get a good box number.\nMake sure to select the given box")
 
-    while len(NeuralNetwork) >= 25:
+    while len(NeuralNetwork) >= 27:
         #print(str(len(NeuralNetwork)))
         sInput = input("Press any key for number...\n")
         os.system('cls')
